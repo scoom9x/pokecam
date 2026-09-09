e6f20c39f488d4daab3ff3f81cb216384b13d0ad
Josh Zhou <joshzhou20080919@gmail.com>
2026-09-08 20:06:35 -0400

    initial commit

---
+++ app/scan/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import {Metadata} from "next"
+import Webcam from "react-webcam"
+import {useRef, useState} from "react"
+
+
+
+
+export default function Scan() {
+    const webcamRef = useRef<Webcam>(null)
+    const [image, setImage] = useState<string | null>(null)
+    function takePicture() {
+        const screenshot = webcamRef.current?.getScreenshot()
+
+        if(screenshot) {
+            setImage(screenshot)
+        }
+    }
+    console.log("hello")
+    return (
+        <div className='h-screen w-screen flex flex-col items-center justify-center'>
+            {!image?<Webcam
+            className="w-full h-full object-cover"
+            ref = {webcamRef}
+            audio = {false}
+            onUserMedia={()=> console.log("camera opened")}
+            onUserMediaError = {(error) => console.log("big error", error)}
+            screenshotFormat="image/jpeg"/>:<img className='w-full h-full object-cover' src={image}/>}
+            <button className="absolute absolute bottom-5" onClick={takePicture}><img className='w-18' src="pokeball.webp"/></button>
+        </div>
+    )
+}
